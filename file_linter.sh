43901b7708aaa2362339d7a173a48c5eceb8af05
OksanaTymchenko <oks.tym@gmail.com>
2021-06-11 17:08:46 +0300

    Add bash script to check file names

---
+++ file_linter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+files_match=true
+for dir in ./datasets/20??/*/; do
+   cd $dir
+   d="${dir:(-3):2}_${dir:(-13):4}"
+   echo "$d"
+   for f in *; do
+      if [[ $f != *"$d"* ]]; then
+         echo "$f MUST NOT be in $dir"
+         files_match=false
+      fi
+      
+   done;
+done;
+if [ "$files_match" = true ]; then
+   echo "All files match their directories"
+else
+   echo "Fix file(s) and push again!"
+   exit 1
+fi
